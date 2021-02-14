0052839e5c1d2da7e234e48e3097e4fc5f07f5a0
ABYtechie <ebyavarachan@gmail.com>
2021-02-14 09:02:33 +0530

    add car and movement

--- GaMe.py
+++ GaMe.py
@@ -1,24 +1,44 @@
 import pygame
 
 pygame.init()
-sienna = (160, 82, 45)
-display_width = 1200
+grey = (119, 118, 110)
+display_width = 1366
 display_height = 700
-gamedisplays = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("ABY's Car Game")
+displays = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("ABBYY's Car Game")
 clock = pygame.time.Clock()
+caring = pygame.image.load("images.jpeg")
+
+
+def car(x, y):
+    displays.blit(caring, (x, y))
+
 
 def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+    xchange = 0
     bumped = False
     while not bumped:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 bumped = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                xchange = -5
+            if event.key == pygame.K_RIGHT:
+                xchange = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT and event.key == pygame.K_RIGHT:
+                continue
+            xchange = 0
+        x += xchange
+        displays.fill(grey)
+        car(x, y)
+        pygame.display.update()
+        clock.tick(60)
 
-                gamedisplays.fill(sienna)
-                pygame.display.update()
-                clock.tick(60)
+
 game_loop()
 pygame.quit()
 quit()
-
